7e43601383821aacf126710f996d9783ed349aab
mhassankhizr <mhassankhizr@gmail.com>
2024-01-18 19:56:16 +0500

    Final

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
+use App\Http\Controllers\PizzaController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicPizzaController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+
+    ]);
 });
+
+
+Route::get('/order/{pizza}', [PublicPizzaController::class, 'show'])->name('public.pizzas.show');
+Route::get('/order/{pizza}/stream', [PublicPizzaController::class, 'stream'])->name('public.pizzas.stream');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/pizzas', [PizzaController::class, 'index'])->name('pizzas.index');
+    Route::get('/pizzas/{pizza}', [PizzaController::class, 'edit'])->name('pizzas.edit');
+    Route::patch('/pizzas/{pizza}', [PizzaController::class, 'update'])->name('pizzas.update');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
